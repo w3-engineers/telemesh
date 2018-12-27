41115faef4eb77538a4de7bc0434a1a60b93c4f0
Mimo <mimosaha@gmail.com>
2018-12-27 15:20:14 +0600

    Add comment

--- app/src/main/java/com/w3engineers/unicef/telemesh/MainActivity.java
+++ app/src/main/java/com/w3engineers/unicef/telemesh/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Functionality added
         TextView helloTextView = findViewById(R.id.hello_text);
 
         InfoParser infoParser = new InfoParser();

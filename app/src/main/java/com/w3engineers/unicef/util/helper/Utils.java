22bc8cb7fb108a315c610ce3aab423f6b88776c4
W3 Mimo <mimosaha@w3engineers.com>
2019-03-05 17:01:35 +0600

    Add non null and nullable annotation for resolving lint problem

--- app/src/main/java/com/w3engineers/unicef/util/helper/Utils.java
+++ app/src/main/java/com/w3engineers/unicef/util/helper/Utils.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.content.pm.ResolveInfo;
 import android.graphics.Color;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Gravity;
@@ -49,6 +51,7 @@ public class Utils {
     private String appFilePath = null;
     private String backupFolder = ".backup";
 
+    @NonNull
     public static Utils getInstance() {
         return utils;
     }
@@ -71,6 +74,7 @@ public class Utils {
      * @return the backup apk path
      */
 
+    @Nullable
     public String backupApkAndGetPath() {
 
         Context context = App.getContext();
@@ -131,7 +135,8 @@ public class Utils {
      * @return - Return alert dialog instance for manually stopping purpose
      */
 
-    public AlertDialog getProgressDialog(Context context) {
+    @NonNull
+    public AlertDialog getProgressDialog(@NonNull Context context) {
 
         int llPadding = 30;
         LinearLayout ll = new LinearLayout(context);

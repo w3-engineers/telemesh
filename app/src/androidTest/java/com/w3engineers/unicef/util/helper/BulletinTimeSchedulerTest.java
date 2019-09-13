267eeadd40d4ebfbe976ff531d991c4ea089f889
Md Tariqul Islam <tariqul@w3engineers.com>
2019-09-13 16:17:02 +0600

    mesh log upload in server unit test done

--- app/src/androidTest/java/com/w3engineers/unicef/util/helper/BulletinTimeSchedulerTest.java
+++ app/src/androidTest/java/com/w3engineers/unicef/util/helper/BulletinTimeSchedulerTest.java
@@ -4,15 +4,26 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
+import android.os.Environment;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v4.content.LocalBroadcastManager;
 
+import com.w3engineers.mesh.MeshApp;
 import com.w3engineers.unicef.telemesh.data.helper.RmDataHelper;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import okhttp3.WebSocketListener;
 
@@ -22,6 +33,7 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class BulletinTimeSchedulerTest {
     Context context;
+    String CURRENT_LOG_FILE_NAME = "testLog.txt";
 
     @Before
     public void setup() {
@@ -31,6 +43,12 @@ public class BulletinTimeSchedulerTest {
     @Test
     public void messageBroadcastTest() {
         BulletinTimeScheduler.NetworkCheckReceiver receiver = BulletinTimeScheduler.getInstance().getReceiver();
+
+        // create temporary file
+        createDummyLogFile("(W) Sample Log 1");
+        addDelay(1000);
+        createDummyLogFile("(S) Sample Log 2");
+        addDelay(1000);
 
         // fake calling in Broadcast
         LocalBroadcastManager.getInstance(context).registerReceiver(receiver, new IntentFilter(Intent.ACTION_PACKAGE_REPLACED));
@@ -62,6 +80,33 @@ public class BulletinTimeSchedulerTest {
         try {
             Thread.sleep(i);
         } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createDummyLogFile(String text) {
+        try {
+            File sdCard = Environment.getExternalStorageDirectory();
+            File directory = new File(sdCard.getAbsolutePath() +
+                    "/MeshRnD");
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+            
+            File file = new File(directory, CURRENT_LOG_FILE_NAME);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileOutputStream fOut = new FileOutputStream(file, true);
+
+            OutputStreamWriter osw = new
+                    OutputStreamWriter(fOut);
+
+            osw.write("\n" + text);
+            osw.flush();
+            osw.close();
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }

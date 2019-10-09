a7484aace8d26c70a26c42832b31cb00e138c001
Md Tariqul Islam <tariqul@w3engineers.com>
2019-10-09 17:53:16 +0600

    chekout commit

--- viper/src/main/java/com/w3engineers/ext/viper/util/lib/mesh/MeshProvider.java
+++ viper/src/main/java/com/w3engineers/ext/viper/util/lib/mesh/MeshProvider.java
@@ -47,7 +47,7 @@ public class MeshProvider implements LinkStateListener {
     /*private String WIFI_PREFIX = "bna.1";
     private String BLE_PREFIX = "que";*/
 
-    private String NETWORK_PREFIX = "hello";
+    private String NETWORK_PREFIX = "abcde";
 
     private MeshProvider() {
         this.context = App.getContext();

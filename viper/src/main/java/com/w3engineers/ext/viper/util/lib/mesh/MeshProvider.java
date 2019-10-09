4ad6f2e9b23ea50777a6e10f751ee3e1ef5c4030
Md Tariqul Islam <tariqul@w3engineers.com>
2019-10-09 17:37:09 +0600

    new SDK integrated

--- viper/src/main/java/com/w3engineers/ext/viper/util/lib/mesh/MeshProvider.java
+++ viper/src/main/java/com/w3engineers/ext/viper/util/lib/mesh/MeshProvider.java
@@ -47,7 +47,7 @@ public class MeshProvider implements LinkStateListener {
     /*private String WIFI_PREFIX = "bna.1";
     private String BLE_PREFIX = "que";*/
 
-    private String NETWORK_PREFIX = "caves";
+    private String NETWORK_PREFIX = "hello";
 
     private MeshProvider() {
         this.context = App.getContext();
@@ -206,16 +206,6 @@ public class MeshProvider implements LinkStateListener {
                 }
             }
         }
-    }
-
-    @Override
-    public void onLocalMessageReceived(String senderId, byte[] frameData) {
-
-    }
-
-    @Override
-    public void onInternetMessageReceived(String senderId, byte[] frameData) {
-
     }
 
     private void peerDiscoveryProcess(String nodeId, boolean isActive) {

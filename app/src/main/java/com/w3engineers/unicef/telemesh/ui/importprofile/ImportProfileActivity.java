9e087e8fd30638603736a9a88d5964a76dc2a549
Md Tariqul Islam <tariqul@w3engineers.com>
2019-11-25 12:28:54 +0600

    feat(Import wallet feature): Existing wallet load functionality and design
       - Exist wallet load page design added
       - Exist wallet address showing in UI. User can understand his/her wallet info
       - Alert message added if user ignore the exist wallet

--- app/src/main/java/com/w3engineers/unicef/telemesh/ui/importprofile/ImportProfileActivity.java
+++ app/src/main/java/com/w3engineers/unicef/telemesh/ui/importprofile/ImportProfileActivity.java
@@ -10,22 +10,37 @@ import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.TextUtils;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 
 import com.w3engineers.ext.strom.application.ui.base.BaseActivity;
 import com.w3engineers.mesh.application.data.local.wallet.WalletService;
+import com.w3engineers.mesh.application.data.local.wallet.Web3jWalletHelper;
+import com.w3engineers.mesh.util.DialogUtil;
 import com.w3engineers.unicef.telemesh.R;
 import com.w3engineers.unicef.telemesh.data.helper.constants.Constants;
 import com.w3engineers.unicef.telemesh.data.provider.ServiceLocator;
 import com.w3engineers.unicef.telemesh.databinding.ActivityImportProfileBinding;
 import com.w3engineers.unicef.telemesh.ui.importwallet.ImportWalletActivity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class ImportProfileActivity extends BaseActivity {
 
     private ActivityImportProfileBinding mBinding;
     private ImportProfileViewModel mViewModel;
     private int PICKFILE_REQUEST_CODE = 480;
+    private String walletSuffixDir;
 
     @Override
     protected int getLayoutId() {
@@ -37,7 +52,7 @@ public class ImportProfileActivity extends BaseActivity {
     protected void startUI() {
         mBinding = (ActivityImportProfileBinding) getViewDataBinding();
         mViewModel = getViewModel();
-
+        walletSuffixDir = "wallet/" + getResources().getString(com.w3engineers.mesh.R.string.app_name);
         initView();
     }
 
@@ -49,7 +64,11 @@ public class ImportProfileActivity extends BaseActivity {
                 finish();
                 break;
             case R.id.button_import_profile:
-                openFileChooser();
+                if (WalletService.getInstance(this).isWalletExists()) {
+                    showWarningDialog();
+                } else {
+                    openFileChooser();
+                }
                 break;
             case R.id.button_continue:
                 // Scan from qr.
@@ -86,11 +105,75 @@ public class ImportProfileActivity extends BaseActivity {
 
         if (WalletService.getInstance(this).isWalletExists()) {
             mBinding.buttonContinue.setVisibility(View.VISIBLE);
-            mBinding.textViewWelcome.setText(getResources().getString(R.string.file_exit));
+            mBinding.textViewWelcome.setText(getWalletSpannableString());
             mBinding.textViewWelcome.setTypeface(mBinding.textViewWelcome.getTypeface(), Typeface.NORMAL);
+            mBinding.textViewWelcome.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
         } else {
             mBinding.buttonContinue.setVisibility(View.INVISIBLE);
+            mBinding.textViewOr.setVisibility(View.INVISIBLE);
+            mBinding.textViewWelcome.setTypeface(mBinding.textViewWelcome.getTypeface(), Typeface.BOLD);
         }
+    }
+
+    private SpannableString getWalletSpannableString() {
+        String walletText = getResources().getString(R.string.file_exit);
+        String address = readWalletAddress();
+
+        String walletExistMessage = String.format(walletText, address);
+        SpannableString spannableString = new SpannableString(walletExistMessage);
+
+
+        spannableString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.wallet_highlight_color)),
+                1, 15, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        int startIndex = walletExistMessage.length() - address.length();
+
+        spannableString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.wallet_highlight_color)),
+                startIndex, walletExistMessage.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        return spannableString;
+    }
+
+    private String readWalletAddress() {
+        String filePath = Web3jWalletHelper.onInstance(this).getWalletDir(walletSuffixDir);
+        File directory = new File(filePath);
+        StringBuilder result = new StringBuilder();
+        File[] list = directory.listFiles();
+        if (list != null) {
+            File walletFile = list[0];
+            if (walletFile.exists()) {
+                FileInputStream fis = null;
+                try {
+                    fis = new FileInputStream(walletFile);
+                    char current;
+                    while (fis.available() > 0) {
+                        current = (char) fis.read();
+                        result.append(current);
+                    }
+                } catch (Exception e) {
+                    Log.e("WalletRead", "Error: " + e.getMessage());
+                } finally {
+                    if (fis != null)
+                        try {
+                            fis.close();
+                        } catch (IOException ignored) {
+                        }
+                }
+            }
+        }
+
+        if (TextUtils.isEmpty(result.toString())) {
+            return result.toString();
+        }
+
+        try {
+            JSONObject walletObj = new JSONObject(result.toString());
+            result = new StringBuilder("0x" + walletObj.optString("address"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
     }
 
     private ImportProfileViewModel getViewModel() {
@@ -109,5 +192,28 @@ public class ImportProfileActivity extends BaseActivity {
         intent.setType(Constants.FILE_TYPE);
         startActivityForResult(intent, PICKFILE_REQUEST_CODE);
 
+    }
+
+    private void showWarningDialog() {
+        DialogUtil.showConfirmationDialog(this,
+                getResources().getString(R.string.warning),
+                getResources().getString(R.string.wallet_lost_warning),
+                getResources().getString(R.string.cancel),
+                getResources().getString(R.string.ok), new DialogUtil.DialogButtonListener() {
+                    @Override
+                    public void onClickPositive() {
+                        openFileChooser();
+                    }
+
+                    @Override
+                    public void onCancel() {
+
+                    }
+
+                    @Override
+                    public void onClickNegative() {
+
+                    }
+                });
     }
 }

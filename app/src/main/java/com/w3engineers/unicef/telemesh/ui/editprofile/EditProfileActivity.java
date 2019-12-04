bee65759fbdf74f3768b4e1345cc1a36fde07824
Md Tariqul Islam <tariqul@w3engineers.com>
2019-12-04 10:54:08 +0600

    feature(Language support):Support English and bangla language
       - Added bangla language support in Feedback page

--- app/src/main/java/com/w3engineers/unicef/telemesh/ui/editprofile/EditProfileActivity.java
+++ app/src/main/java/com/w3engineers/unicef/telemesh/ui/editprofile/EditProfileActivity.java
@@ -6,12 +6,8 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-
-import com.w3engineers.ext.strom.application.ui.base.BaseActivity;
 import com.w3engineers.ext.strom.util.helper.Toaster;
 import com.w3engineers.mesh.application.data.BaseServiceLocator;
 import com.w3engineers.mesh.application.ui.base.TelemeshBaseActivity;

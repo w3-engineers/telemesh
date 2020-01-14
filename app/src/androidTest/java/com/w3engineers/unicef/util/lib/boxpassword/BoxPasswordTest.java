9147dcd13fb3ec6880d65e99d821f98c3a4e6d0f
Md Tariqul Islam <tariqul@w3engineers.com>
2020-01-14 17:15:37 +0600

    fix(Release) - Improve code coverage    - Wallet helper test updated    - Box password test updated

---
+++ app/src/androidTest/java/com/w3engineers/unicef/util/lib/boxpassword/BoxPasswordTest.java
@@ -0,0 +1,49 @@
+package com.w3engineers.unicef.util.lib.boxpassword;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class BoxPasswordTest {
+    private String defaultPass = "123456789101";
+    private Context mContext;
+
+    @Before
+    public void setUp() {
+        mContext = InstrumentationRegistry.getContext();
+    }
+
+    @Test
+    public void boxPasswordEventTest(){
+
+        BoxPassword password =new BoxPassword(mContext);
+
+
+        password.setOnClickListener(v -> {
+            addDelay();
+            password.setText(defaultPass);
+        });
+
+        addDelay();
+
+        password.setText(" 123 567 ");
+
+        addDelay();
+    }
+
+    private void addDelay() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}

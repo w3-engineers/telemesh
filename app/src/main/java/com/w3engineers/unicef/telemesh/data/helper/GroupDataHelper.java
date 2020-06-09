bcbe05ecde71565f899336278853a64ee2098480
W3 Mimo <mimosaha@w3engineers.com>
2020-06-09 18:22:45 +0600

    fix(Release beta 0.1.2): - GroupHelper added for maintaining info

---
+++ app/src/main/java/com/w3engineers/unicef/telemesh/data/helper/GroupDataHelper.java
@@ -0,0 +1,18 @@
+package com.w3engineers.unicef.telemesh.data.helper;
+
+import android.support.annotation.NonNull;
+
+public class GroupDataHelper extends RmDataHelper {
+
+    private static GroupDataHelper groupDataHelper = new GroupDataHelper();
+
+    private GroupDataHelper() {
+
+    }
+
+    @NonNull
+    public static GroupDataHelper getInstance() {
+        return groupDataHelper;
+    }
+
+}

3b655586934a0283d8c525865c14a96b0498f826
Anjan Debnath <anjan@w3engineers.com>
2021-03-12 15:36:45 +0600

    # Feature
    - kotlin plugin installed
    - Splash Activity is now converted to Kotlin
    - Kotlin view extension added
    - No findViewById is required to load any resource

---
+++ app/src/main/java/com/w3engineers/unicef/telemesh/ui/splashscreen/SplashActivityK.kt
@@ -0,0 +1,72 @@
+package com.w3engineers.unicef.telemesh.ui.splashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import com.facebook.shimmer.ShimmerFrameLayout
+import com.w3engineers.unicef.telemesh.R
+import com.w3engineers.unicef.telemesh.data.helper.MeshDataSource
+import com.w3engineers.unicef.telemesh.data.provider.ServiceLocator
+import com.w3engineers.unicef.telemesh.databinding.ActivitySplashBinding
+import com.w3engineers.unicef.telemesh.ui.main.MainActivity
+import com.w3engineers.unicef.telemesh.ui.profilechoice.ProfileChoiceActivity
+import com.w3engineers.unicef.util.helper.CommonUtil
+import com.w3engineers.walleter.wallet.WalletService
+import timber.log.Timber
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivityK: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val splashViewModel = getViewModel()
+        val activitySplashBinding: ActivitySplashBinding = DataBindingUtil.setContentView(this,
+                R.layout.activity_splash)
+
+        activitySplashBinding.setSplashViewModel(splashViewModel)
+
+        MeshDataSource.isPrepared = false
+
+        splashViewModel.getUserRegistrationStatus()
+
+        //val shimmerFrameLayout = findViewById<ShimmerFrameLayout>(R.id.shimmerFrameLayout)
+        // no findViewById required now
+        shimmerFrameLayout.startShimmer()
+
+
+        if (CommonUtil.isEmulator()) {
+            WalletService.getInstance(this).deleteExistsWallet()
+        }
+
+
+        splashViewModel.isUserRegistered.observe(this, { aBoolean: Boolean? ->
+            val intent: Intent
+            intent = if (aBoolean != null && aBoolean) {
+                // Go to contact page
+                Timber.d("User already created. Go next page")
+                Intent(this, MainActivity::class.java)
+            } else {
+                Timber.e("User not created. Go User create page")
+                Intent(this, ProfileChoiceActivity::class.java)
+            }
+            startActivity(intent)
+            finish()
+        })
+    }
+
+
+
+    fun getViewModel(): SplashViewModel{
+        return ViewModelProviders.of(this, object : ViewModelProvider.Factory {
+
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return ServiceLocator.getInstance().getSplashViewModel(application) as T
+            }
+        }).get(SplashViewModel::class.java)
+    }
+}

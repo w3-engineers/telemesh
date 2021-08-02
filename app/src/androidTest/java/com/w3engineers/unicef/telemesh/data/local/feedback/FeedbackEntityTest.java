5d8a8a6167c279aa38041113fd2e8d0a2d4fb822
Tariqul <tariqul@w3engineers.com>
2021-08-02 23:51:37 +0600

    #Fix - Travis issue 15

---
+++ app/src/androidTest/java/com/w3engineers/unicef/telemesh/data/local/feedback/FeedbackEntityTest.java
@@ -0,0 +1,52 @@
+package com.w3engineers.unicef.telemesh.data.local.feedback;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+
+import com.w3engineers.unicef.telemesh.ui.aboutus.AboutUsActivity;
+
+@RunWith(AndroidJUnit4.class)
+public class FeedbackEntityTest {
+    private String userId;
+    private String userName;
+    private String feedback;
+
+    @Rule
+    public ActivityTestRule<AboutUsActivity> rule = new ActivityTestRule<>(AboutUsActivity.class);
+
+    @Before
+    public void setup() {
+        userId = UUID.randomUUID().toString();
+        userName = "John Doe";
+        feedback = "This is sample feedback.";
+    }
+
+    @Test
+    public void feedbackConvertTest() {
+        FeedbackModel model = getFeedbackModel();
+
+        FeedbackEntity feedbackEntity = FeedbackEntity.toFeedbackEntity(model);
+
+        FeedbackModel convertedModel = feedbackEntity.toFeedbackModel();
+
+        assertEquals(convertedModel.getUserId(), model.getUserId());
+    }
+
+    private FeedbackModel getFeedbackModel() {
+        FeedbackModel model = new FeedbackModel();
+        model.setUserId(userId);
+        model.setUserName(userName);
+        model.setFeedbackId(UUID.randomUUID().toString());
+        model.setFeedback(feedback);
+
+        return model;
+    }
+}
---
+++ app/src/androidTest/java/com/w3engineers/unicef/telemesh/data/local/meshlog/MeshLogDataSourceTest.java
@@ -0,0 +1,91 @@
+package com.w3engineers.unicef.telemesh.data.local.meshlog;
+
+import androidx.room.Room;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import android.content.Context;
+
+import com.w3engineers.unicef.telemesh.data.local.db.AppDatabase;
+import com.w3engineers.unicef.telemesh.data.local.feed.FeedDataSource;
+import com.w3engineers.unicef.telemesh.data.local.feed.FeedEntity;
+import com.w3engineers.unicef.telemesh.data.local.feedback.FeedbackDataSource;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class MeshLogDataSourceTest {
+
+    MeshLogDataSource SUT;
+    private AppDatabase appDatabase;
+    private Context mContext;
+
+    private FeedbackDataSource feedbackDataSource;
+    private FeedDataSource feedDataSource;
+
+    @Before
+    public void setup() {
+        mContext = InstrumentationRegistry.getContext();
+
+        appDatabase = Room.inMemoryDatabaseBuilder(mContext,
+                AppDatabase.class).allowMainThreadQueries().build();
+        SUT = MeshLogDataSource.getInstance();
+        feedbackDataSource = FeedbackDataSource.getInstance();
+        feedDataSource  = FeedDataSource.getInstance();
+    }
+
+    @After
+    public void tearDown() {
+        appDatabase.close();
+    }
+
+    @Test
+    public void dataInsertionErrorTest() {
+        addDelay(500);
+
+        long res = SUT.insertOrUpdateData(null);
+
+        addDelay(1000);
+
+        assertEquals(0, res);
+
+        addDelay(500);
+
+        res = feedbackDataSource.insertOrUpdate(null);
+
+        addDelay(1000);
+
+        assertEquals(0, res);
+
+        addDelay(500);
+
+        FeedEntity r = feedDataSource.insertOrUpdateData(null);
+
+        addDelay(1000);
+
+        assertEquals(null, r);
+
+        addDelay(500);
+
+        res = feedDataSource.updateFeedMessageReadStatus(null);
+
+        addDelay(1000);
+
+        assertEquals(0, res);
+
+        addDelay(500);
+    }
+
+    private void addDelay(long time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}

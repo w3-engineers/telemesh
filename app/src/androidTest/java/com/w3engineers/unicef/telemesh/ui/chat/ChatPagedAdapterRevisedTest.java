1671391e9688067897fb294f2a64a680c175af93
W3 Tariqul <tariqul@w3engineers.com>
2021-11-09 13:15:21 +0600

    #Improvement - Group page test updated - Chat page test case updated

--- app/src/androidTest/java/com/w3engineers/unicef/telemesh/ui/chat/ChatPagedAdapterRevisedTest.java
+++ app/src/androidTest/java/com/w3engineers/unicef/telemesh/ui/chat/ChatPagedAdapterRevisedTest.java
@@ -1,17 +1,26 @@
 package com.w3engineers.unicef.telemesh.ui.chat;
 
+import static androidx.test.InstrumentationRegistry.getInstrumentation;
+import static androidx.test.runner.lifecycle.Stage.RESUMED;
 import static org.junit.Assert.*;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 
 import androidx.paging.PagedList;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
 
+import com.w3engineers.unicef.telemesh.R;
 import com.w3engineers.unicef.telemesh.data.helper.constants.Constants;
 import com.w3engineers.unicef.telemesh.data.local.messagetable.ChatEntity;
 import com.w3engineers.unicef.telemesh.data.local.messagetable.GroupMessageEntity;
 import com.w3engineers.unicef.telemesh.data.local.messagetable.MessageEntity;
+import com.w3engineers.unicef.telemesh.data.local.usertable.UserDataSource;
+import com.w3engineers.unicef.telemesh.data.local.usertable.UserEntity;
 import com.w3engineers.unicef.telemesh.data.pager.ChatEntityListDataSource;
 import com.w3engineers.unicef.telemesh.data.pager.MainThreadExecutor;
 import com.w3engineers.unicef.telemesh.ui.aboutus.AboutUsActivity;
@@ -24,27 +33,37 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.Executors;
 
 public class ChatPagedAdapterRevisedTest {
 
+    private String userId = "0xaa2dd785fc60eeb8151f65b3ded59ce3c2f12ca4";
     @Rule
-    public ActivityTestRule<AboutUsActivity> rule = new ActivityTestRule<>(AboutUsActivity.class);
+    public ActivityTestRule<ChatActivity> rule = new ActivityTestRule<ChatActivity>(ChatActivity.class) {
+
+        @Override
+        protected Intent getActivityIntent() {
+            Intent intent = new Intent();
+            intent.putExtra(UserEntity.class.getName(), userId);
+            return intent;
+        }
+    };
     private ChatPagedAdapterRevised adapterRevised;
     private Context mContext;
     private static ChatViewModel SUT = null;
     private static final int INITIAL_LOAD_KEY = 0;
     private static final int PAGE_SIZE = 70;
     private static final int PREFETCH_DISTANCE = 30;
-
+    private UserDataSource userDataSource;
 
     @Before
     public void setUp() throws Exception {
         mContext = InstrumentationRegistry.getTargetContext();
-        SUT = new ChatViewModel(rule.getActivity().getApplication());
-
-        adapterRevised = new ChatPagedAdapterRevised(mContext, SUT, null);
+        //SUT = new ChatViewModel(rule.getActivity().getApplication());
+        userDataSource = UserDataSource.getInstance();
+        addSampleUser();
     }
 
     @After
@@ -56,14 +75,21 @@ public class ChatPagedAdapterRevisedTest {
 
         addDelay(1000);
 
+        currentActivity = getActivityInstance();
+
+        RecyclerView recyclerView = currentActivity.findViewById(R.id.chat_rv);
+        adapterRevised = (ChatPagedAdapterRevised) recyclerView.getAdapter();
+
         MessageEntity messageVideoIncomingEntity = new MessageEntity();
         messageVideoIncomingEntity.setIncoming(true);
+        messageVideoIncomingEntity.setContentPath("path");
         messageVideoIncomingEntity.setMessageType(Constants.MessageType.VIDEO_MESSAGE);
         messageVideoIncomingEntity.setStatus(Constants.MessageStatus.STATUS_FAILED);
         messageVideoIncomingEntity.setFriendsId("userId");
 
         MessageEntity messageVideoOutgoingEntity = new MessageEntity();
         messageVideoOutgoingEntity.setIncoming(false);
+        messageVideoOutgoingEntity.setContentPath("path");
         messageVideoOutgoingEntity.setMessageType(Constants.MessageType.VIDEO_MESSAGE);
         messageVideoOutgoingEntity.setStatus(Constants.MessageStatus.STATUS_FAILED);
         messageVideoOutgoingEntity.setFriendsId("userId");
@@ -112,7 +138,7 @@ public class ChatPagedAdapterRevisedTest {
         chatList.add(groupVideoOutEntity);
 
 
-        addDelay(1000);
+        addDelay(4000);
 
         ChatEntityListDataSource chatEntityListDataSource = new ChatEntityListDataSource(chatList);
 
@@ -136,11 +162,41 @@ public class ChatPagedAdapterRevisedTest {
         StatusHelper.out("Test message_incoming_outgoing_test executed");
     }
 
+    private UserEntity addSampleUser() {
+        UserEntity userEntity = new UserEntity()
+                .setAvatarIndex(1)
+                .setOnlineStatus(Constants.UserStatus.WIFI_MESH_ONLINE)
+                .setMeshId("0xaa2dd785fc60eeb8151f65b3ded59ce3c2f12ca4")
+                .setUserName("Daniel")
+                .setUserLastName("Craig")
+                .setIsFavourite(Constants.FavouriteStatus.UNFAVOURITE)
+                .setRegistrationTime(System.currentTimeMillis());
+        //userEntity.setId(0);
+
+        userDataSource.insertOrUpdateData(userEntity);
+
+        return userEntity;
+    }
+
     private void addDelay(int i) {
         try {
             Thread.sleep(i);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    public Activity currentActivity = null;
+
+    public Activity getActivityInstance() {
+        getInstrumentation().runOnMainSync(() -> {
+            Collection resumedActivities =
+                    ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(RESUMED);
+            if (resumedActivities.iterator().hasNext()) {
+                currentActivity = (Activity) resumedActivities.iterator().next();
+            }
+        });
+
+        return currentActivity;
     }
 }

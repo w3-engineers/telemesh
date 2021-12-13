82a1b5f954e171596c4153b52bb2e95a5c79c4b8
anjan debnath <anjan@w3engineers.com>
2021-12-13 11:10:26 +0600

    [skip travis] fix(activity): add activity instance

--- app/src/androidTest/java/com/w3engineers/unicef/telemesh/ui/selectaccount/SelectAccountActivityTest.java
+++ app/src/androidTest/java/com/w3engineers/unicef/telemesh/ui/selectaccount/SelectAccountActivityTest.java
@@ -1,4 +1,58 @@
 package com.w3engineers.unicef.telemesh.ui.selectaccount;
 
+import static androidx.test.InstrumentationRegistry.getInstrumentation;
+import static androidx.test.runner.lifecycle.Stage.RESUMED;
+
+import android.app.Activity;
+import android.content.Context;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
+import androidx.test.uiautomator.UiDevice;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+import java.util.Collection;
+
+@RunWith(AndroidJUnit4.class)
 public class SelectAccountActivityTest {
+
+    @Rule
+    public ActivityTestRule<SelectAccountActivity> mActivityTestRule = new ActivityTestRule<>(SelectAccountActivity.class);
+    public UiDevice mDevice = UiDevice.getInstance(getInstrumentation());
+    public Activity currentActivity;
+    private Context context;
+
+    @Before
+    public void setUp() {
+        context = InstrumentationRegistry.getTargetContext();
+        currentActivity = getActivityInstance();
+    }
+
+    public void tearDown() {
+    }
+
+    private void addDelay(int i) {
+        try {
+            Thread.sleep(i);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Activity getActivityInstance() {
+        getInstrumentation().runOnMainSync(() -> {
+            Collection resumedActivities =
+                    ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(RESUMED);
+            if (resumedActivities.iterator().hasNext()) {
+                currentActivity = (Activity) resumedActivities.iterator().next();
+            }
+        });
+
+        return currentActivity;
+    }
 }

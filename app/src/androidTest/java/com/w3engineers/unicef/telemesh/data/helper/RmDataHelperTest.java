49f607aa91b1f78d612f5a1f533c74ce644beec4
W3-Mimo <mimosaha@w3engineers.com>
2021-12-15 12:50:37 +0600

    [skip travis] fix(datahelper): new test case for Mesh data helper

--- app/src/androidTest/java/com/w3engineers/unicef/telemesh/data/helper/RmDataHelperTest.java
+++ app/src/androidTest/java/com/w3engineers/unicef/telemesh/data/helper/RmDataHelperTest.java
@@ -1,4 +1,52 @@
 package com.w3engineers.unicef.telemesh.data.helper;
 
+import static org.junit.Assert.assertTrue;
+
+import androidx.test.runner.AndroidJUnit4;
+
+import com.w3engineers.unicef.telemesh.data.helper.constants.Constants;
+import com.w3engineers.unicef.telemesh.data.local.feed.FeedDataSource;
+import com.w3engineers.unicef.telemesh.data.local.feedback.FeedbackDataSource;
+import com.w3engineers.unicef.telemesh.data.local.usertable.UserDataSource;
+import com.w3engineers.unicef.telemesh.util.RandomEntityGenerator;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
 public class RmDataHelperTest {
+
+    private String meshId;
+    private RandomEntityGenerator randomEntityGenerator;
+    private FeedDataSource feedDataSource;
+    private FeedbackDataSource feedbackDataSource;
+    private UserDataSource userDataSource;
+
+    @Before
+    public void setUp() {
+        meshId = "0x550de922bec427fc1b279944e47451a89a4f7car";
+
+        feedDataSource = FeedDataSource.getInstance();
+
+        feedbackDataSource = FeedbackDataSource.getInstance();
+
+        userDataSource = UserDataSource.getInstance();
+
+        randomEntityGenerator = new RandomEntityGenerator();
+    }
+
+    @Test
+    public void testAppUpdateBlocker(){
+        RmDataHelper.getInstance().appUpdateFromOtherServer(Constants.AppUpdateType.BLOCKER,
+                "");
+        assertTrue(true);
+    }
+
+    @Test
+    public void testAppUpdateNoChange(){
+        RmDataHelper.getInstance().appUpdateFromOtherServer(Constants.AppUpdateType.NO_CHANGES,
+                "");
+        assertTrue(true);
+    }
 }

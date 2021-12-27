59599b6215e5b79de29cdb7a866cef992c1cdb4a
W3-Mimo <mimosaha@w3engineers.com>
2021-12-27 16:57:22 +0600

    [skip travis] fix(connectivity_null): Connectivity null check

--- app/src/androidTest/java/com/w3engineers/unicef/util/helper/ConnectivityUtilTest.java
+++ app/src/androidTest/java/com/w3engineers/unicef/util/helper/ConnectivityUtilTest.java
@@ -1,9 +1,30 @@
 package com.w3engineers.unicef.util.helper;
 
+import android.content.Context;
+
+import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import io.reactivex.functions.BiConsumer;
 
 @RunWith(AndroidJUnit4.class)
 public class ConnectivityUtilTest {
+
+    private Context mContext;
+    @Before
+    public void setUp() throws Exception {
+        mContext = InstrumentationRegistry.getTargetContext();
+    }
+
+    @Test
+    public void testInternetException(){
+        BiConsumer<String, Boolean> biConsumer = (key, value) ->
+                System.out.println("Key:"+ key+" Value:"+ value);
+        ConnectivityUtil.isInternetAvailable(mContext,null);
+
+    }
 }

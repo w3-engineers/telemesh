1173ace8b1aa9ce8de4169fcbdb238f758763ccd
W3-Aziz <azizul@w3engineers.com>
2021-12-29 17:36:03 +0600

    [skip travis] fix(meshdata): setup method for test class

--- app/src/androidTest/java/com/w3engineers/unicef/util/helper/MeshDataSourceTest.java
+++ app/src/androidTest/java/com/w3engineers/unicef/util/helper/MeshDataSourceTest.java
@@ -2,10 +2,15 @@ package com.w3engineers.unicef.util.helper;
 
 import androidx.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class MeshDataSourceTest {
 
+    @Before
+    public void setUp() throws Exception {
+
+    }
 
 }

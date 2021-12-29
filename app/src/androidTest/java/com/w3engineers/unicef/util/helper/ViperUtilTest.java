45adc360b1ff35a8f98a25c6d3185b76f8828a37
anjan debnath <anjan@w3engineers.com>
2021-12-29 13:41:09 +0600

    fix(viperutil): extends the required methods

--- app/src/androidTest/java/com/w3engineers/unicef/util/helper/ViperUtilTest.java
+++ app/src/androidTest/java/com/w3engineers/unicef/util/helper/ViperUtilTest.java
@@ -1,4 +1,106 @@
 package com.w3engineers.unicef.util.helper;
 
+
+import androidx.test.runner.AndroidJUnit4;
+
+import com.w3engineers.unicef.telemesh.data.helper.ContentPendingModel;
+import com.w3engineers.unicef.telemesh.data.local.usertable.UserModel;
+import com.w3engineers.unicef.util.helper.model.ViperData;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
 public class ViperUtilTest {
+
+    class CommonViper extends ViperUtil{
+
+        protected CommonViper(UserModel userModel) {
+            super(userModel);
+        }
+
+        @Override
+        protected void onMesh(String myMeshId) {
+
+        }
+
+        @Override
+        protected void onMeshPrepared(String myWalletAddress) {
+
+        }
+
+        @Override
+        protected void offMesh() {
+
+        }
+
+        @Override
+        protected void peerAdd(String peerId, byte[] peerData) {
+
+        }
+
+        @Override
+        protected void peerAdd(String peerId, UserModel userModel) {
+
+        }
+
+        @Override
+        protected void peerRemove(String nodeId) {
+
+        }
+
+        @Override
+        protected void onData(String peerId, ViperData viperData) {
+
+        }
+
+        @Override
+        protected void onAck(String messageId, int status) {
+
+        }
+
+        @Override
+        protected boolean isNodeAvailable(String nodeId, int userActiveStatus) {
+            return false;
+        }
+
+        @Override
+        protected void contentReceiveStart(String contentId, String contentPath, String userId, byte[] metaData) {
+
+        }
+
+        @Override
+        protected void contentReceiveInProgress(String contentId, int progress) {
+
+        }
+
+        @Override
+        protected void contentReceiveDone(String contentId, boolean contentStatus, String msg) {
+
+        }
+
+        @Override
+        protected void pendingContents(ContentPendingModel contentPendingModel) {
+
+        }
+
+        @Override
+        protected void receiveBroadcast(String broadcastId, String metaData, String contentPath, double latitude, double longitude, double range, String expiryTime) {
+
+        }
+
+        @Override
+        protected void openSelectAccountActivity() {
+
+        }
+
+        @Override
+        protected void onWalletPrepared(boolean isOldAccount, boolean isImportWallet) {
+
+        }
+
+        @Override
+        protected void onWalletBackUp(boolean isSuccess) {
+
+        }
+    }
 }

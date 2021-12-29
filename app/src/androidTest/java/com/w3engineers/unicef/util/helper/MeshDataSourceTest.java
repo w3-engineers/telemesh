42e9081003f71b5be798ae5a5d337019607be0bb
W3-Aziz <azizul@w3engineers.com>
2021-12-29 17:32:35 +0600

    [skip travis] fix(meshdata): mesh data source test class MeshDataSourceTest.java

---
+++ app/src/androidTest/java/com/w3engineers/unicef/util/helper/MeshDataSourceTest.java
@@ -0,0 +1,11 @@
+package com.w3engineers.unicef.util.helper;
+
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MeshDataSourceTest {
+
+
+}

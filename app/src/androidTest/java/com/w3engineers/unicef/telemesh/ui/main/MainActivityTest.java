24b8f5cc950ac6f90407af77f39cf0727260f488
W3-Mimo <mimosaha@w3engineers.com>
2022-01-03 12:10:45 +0600

    [skip travis] fix(main): test rule add on main activity test class

--- app/src/androidTest/java/com/w3engineers/unicef/telemesh/ui/main/MainActivityTest.java
+++ app/src/androidTest/java/com/w3engineers/unicef/telemesh/ui/main/MainActivityTest.java
@@ -1,9 +1,17 @@
 package com.w3engineers.unicef.telemesh.ui.main;
 
+import android.content.Context;
+
+import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    private Context context;
 }

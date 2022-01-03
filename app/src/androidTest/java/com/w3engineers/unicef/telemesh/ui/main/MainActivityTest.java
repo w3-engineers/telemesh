94bff69c1d4b46b0ead4e5f81fad7cde3462a1b6
W3-Mimo <mimosaha@w3engineers.com>
2022-01-03 12:11:48 +0600

    [skip travis] fix(main): setup and teardown class include

--- app/src/androidTest/java/com/w3engineers/unicef/telemesh/ui/main/MainActivityTest.java
+++ app/src/androidTest/java/com/w3engineers/unicef/telemesh/ui/main/MainActivityTest.java
@@ -2,9 +2,12 @@ package com.w3engineers.unicef.telemesh.ui.main;
 
 import android.content.Context;
 
+import androidx.test.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 
@@ -14,4 +17,13 @@ public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
     private Context context;
+
+    @Before
+    public void setUp() {
+        context = InstrumentationRegistry.getTargetContext();
+    }
+
+    @After
+    public void tearDown() {
+    }
 }
